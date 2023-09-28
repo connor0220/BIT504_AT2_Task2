e6436e9b2edab41e1aa1b537effb31f27ab9e2ec
connor0220 <145977405+connor0220@users.noreply.github.com>
2023-09-28 13:43:47 +1300

    Update Cell.java
    Set the value of content to Empty

--- Cell.java
+++ Cell.java
@@ -43,10 +43,8 @@ public class Cell {
 	
 	/** Set this cell's content to EMPTY */
 	public void clear() {
-		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
-		
+		// Set the value of content to Empty (Remember this is an enum)
+        	content = Player.Empty;
 	}
 		
 }
