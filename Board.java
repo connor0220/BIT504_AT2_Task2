24978a62edb281e49d17b527849fc107ff7d5af1
connor0220 <145977405+connor0220@users.noreply.github.com>
2023-09-28 13:38:48 +1300

    Update Board.java
    Checked if player has 3 in a row, including diagonal

--- Board.java
+++ Board.java
@@ -27,21 +27,15 @@ public class Board {
 	 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
 	public boolean isDraw() {
 		 
-		// TODO: Check whether the game has ended in a draw. 
-		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
-		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-	for (int row = 0; row < GameMain.ROWS; ++row) {
-            for (int col = 0; col < GameMain.COLS; ++col) {
-                if (cells[row][col].content == Player.Empty) {
-                    return false; // There's an empty cell, so it's not a draw.
-                }
-            }
-        }
-        return true; // All cells are filled, so it's a draw.
-		   
-		
-
-		
+		for (int row = 0; row < GameMain.ROWS; ++row) {
+	            for (int col = 0; col < GameMain.COLS; ++col) {
+	                if (cells[row][col].content == Player.Empty) {
+	                    return false; // There's an empty cell, so it's not a draw.
+	                }
+		    }
+			
+        	}
+        return true; // All cells are filled, so it's a draw.	
 	}
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
@@ -50,20 +44,18 @@ public class Board {
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		
-		
+		// Check if the player has 3 in the playerCol
+	        if (cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
+	            return true;
+	
+	        // Check the diagonal in the other direction
+	        if (cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer)
+	            return true;
 		
 		 // 3-in-the-diagonal
 		if( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
 			return true;
 		 
-		
-		// TODO: Check the diagonal in the other direction
-		
-
-		
 		//no winner, keep playing
 		return false;
 	}
