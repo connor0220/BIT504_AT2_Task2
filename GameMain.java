32afccc6d081f825a5a63f437bef9e5da83f3500
connor0220 <145977405+connor0220@users.noreply.github.com>
2023-09-28 13:59:09 +1300

    Update GameMain.java
    Added JPanel MouseEvent on MouseClicked event

--- GameMain.java
+++ GameMain.java
@@ -39,8 +39,8 @@ public class GameMain extends JPanel implements MouseListener{
 	/** Constructor to setup the UI and game components on the panel */
 	public GameMain() {   
 		
-		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.          
-	    
+		// This JPanel fires a MouseEvent on MouseClicked so add the required event listener to 'this'.
+        	addMouseListener(this);          
 	    
 		// Setup the status bar (JLabel) to display status message       
 		statusBar = new JLabel("         ");       
